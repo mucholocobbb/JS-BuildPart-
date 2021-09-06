2abf082b721951e6365e7d3e93169f8954fd2bb6
Egor <brodiaga89@gmail.com>
2021-09-06 17:50:53 +0300

    +Some

--- src/loadScript.js
+++ src/loadScript.js
@@ -2,18 +2,29 @@ import {
     getTemlate
 } from "./templGen.js"
 
-function loadScript(url) {
-    let test = document.querySelector('.test')
+
+export function loadScript(url, atr) {
     const element = document.createElement("script");
-    element.src = url;
-    element.integrity = "sha512-dMiBqqEV5i3KqpAdsgQSSZ72DnNib7otY0X+b/Tnn1EmLym1dxuZ19csdZqBnfDniKd9bmNtzStQzuMrIa5Ngw==";
-    element.setAttribute("crossorigin", "anonymous");
-    element.setAttribute("referrerpolicy", "no-referrer");
-    test.replaceWith(element);
-    console.log(element);
+    if (atr === 1) {
+        let test = document.querySelector('.test')
+        element.src = url;
+        element.integrity = "sha512-dMiBqqEV5i3KqpAdsgQSSZ72DnNib7otY0X+b/Tnn1EmLym1dxuZ19csdZqBnfDniKd9bmNtzStQzuMrIa5Ngw==";
+        element.setAttribute("crossorigin", "anonymous");
+        element.setAttribute("referrerpolicy", "no-referrer");
+        test.replaceWith(element);
+        console.log(element);
+    } else if (atr === 2) {
+        let test = document.querySelector('.test2')
+        element.setAttribute("defer", "");
+        element.src = url;
+        test.replaceWith(element);
+        console.log(element);
+    }
+
 }
 
 export const mainBlock = document.querySelector('.mainTempl');
 
-loadScript('https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.3/howler.js');
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.3/howler.js', 1);
+loadScript('https://cdn.jsdelivr.net/npm/luxon@1.25.0/build/global/luxon.min.js', 2);
 getTemlate();
